698606357f16edfe0c93cd020913d12c93b062c5
Javier Gonzalez <tezukazone008@gmail.com>
2024-07-06 14:43:11 -0300

    Modelo para Entity Framework Core

---
+++ MagicVilla_API/Data/VillaStore.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_API.Models.Dto;
+
+namespace MagicVilla_API.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDto> villaList = new List<VillaDto> 
+        {
+            new VillaDto{Id = 1, Nombre = "Vista a la piscina", MetrosCuadrados = 100, Ocupantes = 1},
+            new VillaDto{Id = 2, Nombre = "Vista a la playa", MetrosCuadrados = 200, Ocupantes = 2},
+
+        };
+    }
+}
